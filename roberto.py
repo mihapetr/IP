8ae23cf1e14e9aca480419b5858c3012e40c58da
mihapetr <miwaque@gmail.com>
2023-06-15 12:36:21 +0200

    dodao interaktivni način rada u roberto.py

--- roberto.py
+++ roberto.py
@@ -968,4 +968,77 @@ def unos_programa(ime_txt_dat):
     
     P(naredbe).izvrši() 
 
-unos_programa("program.txt") # da ne moramo stalno u VSC pisati program vec u .txt
+# unos_programa("program.txt") # da ne moramo stalno u VSC pisati program vec u .txt
+
+######################### interaktivni način rada ###############################
+
+def main() :
+
+    # ispis uputa
+    print(10 * "-" + " MODALNA LOGIKA " + 10 * "-")
+    print("Naredbe završavaju znakom ';'.")
+    print("Za izvršavanje unesenog bloka dvaput pritisnite Enter.")
+    print("Unesite 'bye' ili 'exit' za kraj rada.")
+    print("'napravi <ime_datoteke>.mir' izvršava skriptu u toj datoteci.")
+    print(60 * "-")
+
+    block = ""
+    while True :
+        print(">", end =" ") # ispisuje oznaku da smo unutar naše ljuske bez prelaska u novi red
+        line = ""
+        line = input() # čeka input korisnika
+
+        # IZLAZ IZ PROGRAMA
+        if line in {"bye", "exit"} :    # kraj rada
+            exit()
+
+        # NAPRAVI SKRIPTU
+        elif line[:7] == "napravi":
+            block = ""  # resetira do sad uneseno
+            s = line.split(' ')
+            script = s[1] # drugi element je naziv datoteke
+
+            if not script[-4:] == ".mir" : 
+                print("Tip datoteke mora biti .mir!")
+                continue
+
+            if not len(s) == 2 :
+                print("Previše argumenata! Koristiti: napravi <ime_datoteke>.mir")
+                continue
+
+            # print("izvršavam skriptu " + script)
+            try : f = open(script, "r")
+            except : print("Greška prilikom otvaranja datoteke. Postoji li?")
+            else :
+                # uspješno otvaranje
+                # print(">", end=" ")
+                try : P(f.read()).izvrši()
+                except Exception as e:
+                    print(e)
+                #print("datoteka: " + f.read())
+                f.close()
+                print("")
+        
+        # DVOSTRUKI ENTER
+        elif line == "" and not block == "" :  # block sadrži odsječak koda
+            #print("blok koda:")   
+            #print(block)
+            try : P(block).izvrši() # izvršavamo kod 
+            except Exception as e :
+                print(e)
+            block = ""       # reset
+            print("")
+
+        # KONKATENACIJA NOVE LINIJE U TRENUTNI BLOK KODA
+        elif not line == "" :  # linija koda unesena
+            block += line + "\n"
+
+        # NEBTINI PRELASCI U NOVI RED
+        elif line == "" :
+            pass    # ignoriramo bezvezno unošenje novih redova
+
+        # CATCHALL DEBUG
+        else :
+            print("nepokriven slučaj!") # za debug; ne bi trebalo doći do toga
+
+main()
